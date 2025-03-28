aa25111ae256984ec8fe8fdf8783ade6fd0bcaec
Yoora <83355885+kimyura29@users.noreply.github.com>
2025-03-28 13:34:43 +0900

    [Bronze V] Title: 새싹, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = input()
+
+print('         ,r\'\"7')
+print('r`-_   ,\'  ,/')
+print(' \. \". L_r\'')
+print('   `~\/')
+print('      |')
+print('      |')
