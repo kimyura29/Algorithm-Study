2f25c2fb2669d92155a7f88ce229a50a8cd5dad5
Yoora <83355885+kimyura29@users.noreply.github.com>
2025-04-09 18:44:26 +0900

    [Bronze I] Title: FizzBuzz, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+for i in range(3, 0, -1):
+    x = input().strip()
+    if x not in ["Fizz", "Buzz", "FizzBuzz"]:
+        n = int(x) + i
+if n%15==0:
+    print("FizzBuzz")
+elif n%3==0:
+    print("Fizz")
+elif n%5==0:
+    print("Buzz")
+else:
+    print(n)
+    
+
+
+
