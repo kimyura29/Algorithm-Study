33e645b3daeb8202e2f3766b8172097c3c24bd7b
Yoora <83355885+kimyura29@users.noreply.github.com>
2025-04-30 15:41:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 57388 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,51 +1,25 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
 
-dq = deque()
-visit = set()
-
-# 위치, 시간 
-dq.append((n,0))
-visit.add(n)
-
-ans = 0
-while dq:
-    # 현재 위치, 시간
-    cx, ct = dq.popleft()
-
-    if cx == k:
-        ans = ct
-        break
-
-    if cx > 100000:
-        continue
-
-    # N+1, N-1, N*2
-    #1. 방문이 가능한지 -> 경계값 체크(가능한지)
-    #2. 중복체크(방문한적이 있는지)
+def solution(n, k):
     
-    # 방문 가능 체크 X
-    # 중복 체크 -> 없으면 반복
+    queue = deque()
+    queue.append((n,0)) # 위치, 시간
+    visit = set()
+    count = 0
+    while queue:
+        cx, ct = queue.popleft()
+        if cx == k:
+            return ct
+        # 경계값 체크
+        if cx > 100000:
+            continue
 
-    nx = cx+1
-    if nx not in visit:
-        # 새로운 지점, 시간+1
-        dq.append((nx, ct+1))
-        visit.add(nx)
+        # 방문이 가능한지, 중복체크
+        for nx in (cx+1, cx-1, 2* cx):
+            # 아직 방문 안했다면
+            if not nx in visit:
+                queue.append((nx,ct + 1))
+                visit.add(nx)
 
-    nx = cx-1
-    if nx not in visit:
-        # 새로운 지점, 시간+1
-        dq.append((nx, ct+1))
-        visit.add(nx)
-
-    nx = cx*2
-    if nx not in visit:
-        # 새로운 지점, 시간+1
-        dq.append((nx, ct+1))
-        visit.add(nx)
-
-print(ct)
+print(solution(n, k))
