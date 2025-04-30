88672c99c59d9c2e148880aa520f23b65f7bd8c7
Yoora <83355885+kimyura29@users.noreply.github.com>
2025-04-30 22:45:49 +0900

    [Silver II] Title: 유기농 배추, Time: 64 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,57 +1,39 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+from collections import deque
+sys.setrecursionlimit(10**6)
 
+# 방향벡터
+dx = [0, 0 ,1, -1]
+dy = [1, -1, 0, 0]
 
-# 방향벡터 설정(상하좌우)
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+def dfs(x, y):
+    visit.add((x,y))
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx <m and 0 <= ny < n:
+            if (nx, ny) not in visit and cabbage[ny][nx] == 1:
+                dfs(nx, ny) # 방문처리는 dfs내에서 함
 
+t = int(input())
 
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x,y))
-    visit.add((x,y)) # 방문 처리
-
-    while queue:
-        cx, cy = queue.popleft()
-
-        # 상하좌우 
-        for i in range(4):
-            nx = cx + dx[i]
-            ny = cy + dy[i]
-
-            # 1. 방문이 가능한지 
-            if nx < 0 or nx >= m or ny <0 or ny >= n:
-                continue
-
-            # 2. 중복 체크 (방문한적이 있는지)
-            if (nx, ny) not in visit and maps[ny][nx] == 1: # 2차원 배열은 [y][x] 순 🌟
-                queue.append((nx, ny))
-                visit.add((nx, ny))
-
-# 테스트 케이스
-t =  int(input())
-
-# 입력 받기
 for _ in range(t):
-    m, n, k =  map(int, input().split()) # m: 가로, n: 세로
-    maps = [[0]*m for _ in range(n)] # [[0,0,0...],..] 이렇게 2차원 리스트로 채움
+    m, n, k = map(int, input().split())
+    cabbage = [[0] * m for _ in range(n)]
+    visit = set()
+    count = 0 # 개수
 
     for _ in range(k):
         x, y = map(int, input().split())
-        maps[y][x] = 1
+        cabbage[y][x] = 1
+
     
-    visit = set() # 테스트 케이스마다 visit 초기화 해줘야 해🌟
-
-    count = 0 # 지렁이수 계산
-    for y in range(n): # 세로 범위(행)
-        for x in range(m): # 가로 범위(열)
-            # 배추가 있고, 아직 방문하지 않았을 때
-            if maps[y][x] == 1 and (x, y) not in visit:
-                bfs(x, y)
+    for i in range(n): # 행
+        for j in range(m): # 열
+            if (j, i) not in visit and cabbage[i][j] == 1:
+                dfs(j,i)
                 count += 1
-        
     print(count)
-
+    
+    
