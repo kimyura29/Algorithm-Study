0be75115faafb1a41c95c61d767fef8d5204a669
Yoora <83355885+kimyura29@users.noreply.github.com>
2025-05-01 14:17:44 +0900

    [Silver I] Title: 영역 구하기, Time: 52 ms, Memory: 35580 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,60 +1,43 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-
-m, n, k = map(int,input().split())
-maps = [[0]*n for _ in range(m)] # n X m 2차원 배열 만들기
-for _ in range(k):
-    x1, y1, x2, y2 = map(int, input().split())
-
-    # # (x1, y1)부터 (x2-1, y2-1)까지 1로 채움 (직사각형 내부 영역)
-    for x in range(x1, x2):
-        for y in range(y1, y2):
-            maps[y][x] = 1 # y는 행, x는 열
-
-# 방문여부
-visit = set()
-
-# 방향 벡터 설정(상 하 우 좌)
+sys.setrecursionlimit(100000)
+# 방향벡터
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
+# DFS로 풀어보기 => 재귀
+visit = set()
+def dfs(x, y):
+    visit.add((x,y))
+    cnt = 1 # 칸수 세기(시작점 포함)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 1. 경계선 체크
+        if 0 <= nx <= n-1 and 0 <= ny <= m-1:
+            # 2. 방문가능한지
+            if (nx, ny) not in visit and maps[ny][nx] == 1:
+                visit.add((nx, ny))
+                cnt += dfs(nx, ny) # dfs돌아가는 횟수만큼 넓이 누적
+    return cnt
 
 
-def bfs(x,y):
-    queue = deque()
-    queue.append((x, y)) # 시작점 삽입
-    visit.add((x,y)) # 시작점 방문처리
-    count = 1 # 현재 영역 넓이 초기화
+m, n, k = map(int,input().split())
+maps = [[1]*n for _ in range(m)] # nXm 으로 2차원 리스트 만들기
+for _ in range(k):
+    x1, y1, x2, y2 = map(int,input().split())
+    for i in range(y1, y2): # 행
+        for j in range(x1, x2): # 열 
+            maps[i][j] = 0 # 색칠된 부분 0으로 채우기
 
-    # 큐가 빌때까지 반복
-    while queue:
-        cx, cy = queue.popleft()
+result = []
+for y in range(m): # 행
+    for x in range(n): # 열 
+        if (x, y) not in visit and maps[y][x]==1:
+            r = dfs(x,y)
+            result.append(r)
 
-        for i in range(4):
-            nx = cx + dx[i]
-            ny = cy + dy[i]
-            
-            # 1. 방문이 가능한지 
-            if nx < 0 or nx >= n or ny <0 or ny >= m:
-                continue
+result.sort()
+print(len(result))
+print(*result)
+    
 
-            # 2. 중복 체크 (방문한적이 있는지)
-            if (nx, ny) not in visit and maps[ny][nx] == 0:
-                queue.append((nx,ny)) # 큐에 추가
-                visit.add((nx, ny)) # 방문 처리
-                count += 1 # 넓이 + 1
-    return count # 해당 영역 넓이 반환
-
-# 각 영역의 넓이 저장
-results = []
-
-for y in range(m):
-    for x in range(n):
-        # 아직 방문하지 않았으면
-        if maps[y][x] == 0 and (x, y) not in visit:
-            area = bfs(x, y) # 해당 영역의 넓이
-            results.append(area)
-
-print(len(results))
-for k in sorted(results):
-    print(k, end=' ')
