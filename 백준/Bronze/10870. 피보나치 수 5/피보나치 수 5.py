1692e34479bac4de85cd6d09e7d7fab7149ba198
Yoora <83355885+kimyura29@users.noreply.github.com>
2025-05-18 11:00:07 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+fibo = [0] * (n+1)
+def sol(n):
+    if n == 0:
+        return 0
+    fibo = [0] * (n+1)
+    fibo[1] = 1
+    for i in range(2, n+1):
+        fibo[i] = fibo[i-1] + fibo[i-2]
+    return fibo[n]
+
+print(sol(n))
