088a0fc5904499e10b0f9aca52ff0dcbf7f03e10
Yoora <83355885+kimyura29@users.noreply.github.com>
2025-05-28 17:45:26 +0900

    [Silver IV] Title: 캠프가는 영식, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.py
+++ 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.py
@@ -5,25 +5,20 @@ bus_time = []
 for _ in range(n):
     s,l,c = map(int,input().split())
     for i in range(c):
-        bus_time.append(s + l * i)
-
+        if s + l * i >= t: # 런타임 에러 방지
+            bus_time.append(s + l * i)
 bus_time.sort() # 정렬
 
-def sol(bus_time, t):
-    left, right = 0, len(bus_time)-1 # 인덱스니까
-    answer = float('inf') # 최소 시간 구하기
-
-    while left <= right:
-        mid = (left + right) // 2
-        # 도착시간 = 버스시간 
-        if bus_time[mid] == t:
+def solution(bus_time, t):
+    time_list = []
+    for i in bus_time:
+        if i == t:
             return 0
-        elif bus_time[mid] > t:
-            answer = min(answer, bus_time[mid])
-            right = mid - 1
-        else:
-            left = mid + 1
-    return answer - t if answer != float('inf') else -1 # 캠프에 갈 수 없으면 -1 출력
-        
+        elif i > t:
+            time_list.append(i-t)
+    if time_list:
+        return min(time_list)
+    else:
+        return -1
 
-print(sol(bus_time, t))
+print(solution(bus_time, t))
